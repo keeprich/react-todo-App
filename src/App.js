f88a9120e8efb733ceda8faa1da57fe3c7162b80
keeprich <keeprich@keeprichs-air.lan>
2020-03-21 22:06:34 +0000

    first Reactjs App two

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+import NavBar from './NavBar';
+import ToDoList from './ToDoList';
+import uuidv4 from 'uuid/v4';
+// import logo from './logo.svg';
 import './App.css';
 
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  // Add effect for storing our todo lists
+
+  useEffect(() => {
+    const storedTodos = JSON.parse( localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+// tick and untick todo lists
+function toggleTodo(id) {
+  const newTodos = [...todos]
+  const todo = newTodos.find(todo =>todo.id === id)
+  todo.complete = !todo.complete
+  setTodos(newTodos)
 }
+
+  function handleAddTodo(e) {
+const name = todoNameRef.current.value
+
+if (name === '') 
+return 
+setTodos(prevTodos => {
+  return [...prevTodos, {id: uuidv4(), name: name, complete:false}]
+})
+todoNameRef.current.value = null
+  }
+
+  function handelAddTodos() {
+    const newTodos =todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
+  return (
+    <>
+    <NavBar/>
+  <ToDoList todos={todos} toggleTodo = {toggleTodo}/>
+  <input ref={todoNameRef} type="text"/>
+<button onClick={handleAddTodo}>Add to Do</button>
+<button onClick = {handelAddTodos}>Clear completed todo</button>
+<div>{todos.filter(todo => !todo.complete).length} Left to do</div>
+  </>
+
+    )
+     
+}
+
+
+
+
 
 export default App;
